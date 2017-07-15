ed06fb7f89679852e4fe5f4246a3d382464fb8bd
Miff Falden <miff@miffthefox.info>
2017-07-15 00:25:10 -0400

    Can now load type and properties from tmx files.

--- Chiamo/Chiamo/TMX/TmxObjectInfo.cs
+++ Chiamo/Chiamo/TMX/TmxObjectInfo.cs
@@ -3,42 +3,70 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace MiffTheFox.Chiamo.TMX
 {
     public sealed class TmxObjectInfo
     {
-        public string Name { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Rectangle Bounds => new Rectangle(X, Y, Width, Height);
+        public bool HasProperties => _Properties != null;
+        public string this[string propertyKey] => (_Properties != null && _Properties.ContainsKey(propertyKey)) ? _Properties[propertyKey] : null;
+
+        private readonly Dictionary<string, string> _Properties;
 
         internal TmxObjectInfo(XmlElement objectElement)
         {
-            Name = objectElement.GetAttribute("name") ?? "";
-            X = 0;
-            Y = 0;
-            int n;
+            Name = objectElement.GetAttribute("name");
+            Type = objectElement.GetAttribute("type");
+            if (string.IsNullOrEmpty(Type)) Type = Name;
 
-            if (int.TryParse(objectElement.GetAttribute("x"), NumberStyles.None, CultureInfo.InvariantCulture, out n))
+            X = _ParseAttr(objectElement, "x");
+            Y = _ParseAttr(objectElement, "y");
+            Width = _ParseAttr(objectElement, "width");
+            Height = _ParseAttr(objectElement, "height");
+
+            if (objectElement.HasAttribute("gid"))
             {
-                X = n;
+                Y -= Height;
             }
 
-            if (int.TryParse(objectElement.GetAttribute("y"), NumberStyles.None, CultureInfo.InvariantCulture, out n))
+            var propertiesElement = objectElement.ChildNodes.Cast<XmlNode>().Where(xn => xn is XmlElement && xn.Name == "properties").FirstOrDefault() as XmlElement;
+            if (propertiesElement == null)
             {
-                Y = n;
+                _Properties = null;
+            }
+            else
+            {
+                _Properties = new Dictionary<string, string>();
 
-                // Tiled uses the origin point of an actor in the bottom left, whereis Chiamo uses the top left.
-                // Therefore, fix the Y value by adding the height the Tiled expected the object to be.
-                // (Turns out this is only for "tile" objects?"
-
-                if (objectElement.HasAttribute("gid") && int.TryParse(objectElement.GetAttribute("height"), NumberStyles.None, CultureInfo.InvariantCulture, out n))
+                foreach (XmlElement propertyElement in propertiesElement.GetElementsByTagName("property"))
                 {
-                    Y -= n;
+                    string name = propertiesElement.GetAttribute("name");
+                    string value = propertiesElement.GetAttribute("value");
+                    _Properties.Add(name, value);
                 }
+            }
+        }
+
+        private static int _ParseAttr(XmlElement element, string attributeName)
+        {
+            string v = element.GetAttribute(attributeName);
+            if (!string.IsNullOrEmpty(v) && float.TryParse(v, NumberStyles.Number, CultureInfo.InvariantCulture, out float f))
+            {
+                return Convert.ToInt32(f);
+            }
+            else
+            {
+                return 0;
             }
         }
     }
